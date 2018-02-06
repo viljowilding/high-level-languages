ec921f20e2983b00dceb268dfef22662b1009416
Viljo Wilding <17092858@woodhouse.ac.uk>
2018-02-06 18:06:18 +0000

    Python & readme
    Readme contains info on high level languages.
    Python file contains the python example and related info as comments.

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-high-level-languages
+# high-level-languages
+
+**Definition:** High-level programming languages are human-readable, syntax based lanugages, 
+designed to be easily understood by humans, rather than by computers.
+Some high-level languages, like Python, read very much like natural english,
+others have stronger syntax requirements
---
+++ python-info.py
@@ -0,0 +1,32 @@
+### PYTHON
+
+'''
+Features:
+- Source code consists of a very basic set of instructions, mostly in natural english, with little syntax.
+- Needs a translator
+  - The most popular Python release, CPython, runs in an IDE which converts
+    source code into to C before running
+- Files usually end in the .py suffix
+- Uses "The Philosophy of Python"
+'''
+
+print("Hello World!")
+
+def whatsYourName(msg):
+	print(msg)
+	temp = input()
+	return temp
+
+name = whatsYourName("What's your name, user?\nName: ")
+if name == ""
+print("{} is such a lovely name!".format(name))
+
+'''
+The Philosophy of Python:
+- Beautiful is better than ugly
+- Explicit is better than implicit
+- Simple is better than complex
+- Complex is better than complicated
+- Readability counts
+'''
+
